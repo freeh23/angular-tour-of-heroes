547189dc55d6ca1cf3fa0db1a0027d98443ea226
Free Hoebeke <freehoebeke@gmail.com>
2021-11-30 21:33:25 +0100

    "Get data from a server" chapter: getHeroes with Http implemented

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,23 +3,31 @@ import {Hero} from "./Hero";
 import {HEROES} from "./mock-heroes";
 import {Observable, of} from "rxjs";
 import {MessageService} from "./messages.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor(private messageService : MessageService) { }
+  constructor(
+    private http : HttpClient,
+    private messageService : MessageService) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   getHeroes() : Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.messageService.add("HeroService: fetched heroes")
-    return heroes;
+    this.log("fetched heroes");
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number) : Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add("HeroService: fetched hero id=${id}");
+    this.log(`fetched hero id=${id}`);
     return of(hero);
   }
 }
